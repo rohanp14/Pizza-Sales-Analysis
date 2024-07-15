683bb33739cd433eacef88b76701c71deb8b8af8
Rohan Patil <rohanppp1232@gmail.com>
2024-07-15 15:27:16 +0530

    Add files via upload

---
+++ Advance.sql
@@ -0,0 +1,54 @@
+-- Calculate the percentage contribution of each pizza type to total revenue.
+
+SELECT 
+    pizza_types.category,
+    ROUND(SUM(order_details.quantity * pizzas.price) / (SELECT 
+                    ROUND(SUM(order_details.quantity * pizzas.price),
+                                2) AS total_sales
+                FROM
+                    order_details
+                        JOIN
+                    pizzas ON pizzas.pizza_id = order_details.pizza_id) * 100,
+            2) AS revenue
+FROM
+    pizza_types
+        JOIN
+    pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+        JOIN
+    order_details ON order_details.pizza_id = pizzas.pizza_id
+GROUP BY pizza_types.category
+ORDER BY revenue DESC;
+
+-- Analyze the cumulative revenue generated over time.
+
+SELECT order_date, SUM(revenue) OVER(ORDER BY order_date) AS Cum_Revenue FROM
+(SELECT 
+    orders.order_date,
+    SUM(order_details.quantity * pizzas.price) AS revenue
+FROM
+    order_details
+        JOIN
+    pizzas ON order_details.pizza_id = pizzas.pizza_id
+        JOIN
+    orders ON orders.order_id = order_details.order_id
+GROUP BY orders.order_date) AS sales;
+
+-- Determine the top 3 most ordered pizza types based on revenue for each pizza category.
+
+select name, revenue, category from 
+(Select category, name, revenue, 
+rank() over(partition by category order by revenue desc) as p 
+from
+(SELECT 
+    pizza_types.category,
+    pizza_types.name,
+    SUM((order_details.quantity) * pizzas.price) AS revenue
+FROM
+    pizza_types
+        JOIN
+    pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+        JOIN
+    order_details ON order_details.pizza_id = pizzas.pizza_id
+GROUP BY pizza_types.category , pizza_types.name) as r) as s
+where p <= 3;
+
